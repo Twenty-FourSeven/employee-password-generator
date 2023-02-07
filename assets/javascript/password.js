7fa7c273853cf9d67be8917b6a5ca082f69fd117
unknown <thomas.james.1@hotmail.com>
2023-02-07 03:00:10 -0800

    add arrays defining special chars, lowercase, uppercase, and numbers

--- assets/javascript/password.js
+++ assets/javascript/password.js
@@ -2,25 +2,101 @@
 
 var CharLength = 8;
 
-
-//arrays to store all possible symbols and letters for password when password is generated 
+//arrays to store possible symbols and letters for password when password is generated
 
 var selectArray = [];
 
 var numbersArray = [];
 
-var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '[', ']', '{', '}', '|', '\', '/',  ';', ':', '<', '>', '?', '.', ',', ''' ];
 
+//special characters, lowercase, uppercase, and numbers available for employee to choose from
+var specialChar = [
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "[",
+  "]",
+  "{",
+  "}",
+  "|",
+  "'",
+  ".",
+];
 
-var lowerCaseArray = [];
+var lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-var upperCaseArray = [];
+var upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-
-
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -28,8 +104,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
